fb17f13923fbe01331122d9550cfd21d1571f58a
Ystas <ystas1205@gmail.com>
2023-11-10 15:28:51 +0300

    commit

---
+++ m2m-relations/articles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Article, Tag, Scope
+
+
+def articles_list(request):
+    template = 'articles/news.html'
+    article = Article.objects.all()
+    context = {'object_list': article}
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    # ordering = '-published_at'
+    return render(request, template, context)
---
+++ orm_migrations/school/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Student, Teacher
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'group']
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'subject']
